3f8566a975c05f158510dca11f3a54d7d80194bc
Daniel DeLorme <daniel@dan42.com>
2020-01-23 11:37:13 -0500

    refactor: args_rest_behead optimization

--- vm_args.c
+++ vm_args.c
@@ -201,8 +201,33 @@ args_check_block_arg0(struct args_info *args)
 }
 
 static inline void
+args_rest_behead(struct args_info *args)
+{
+    if (args->rest_index) {
+        VM_ASSERT(args->argc == 0);
+        if (args->rest_dupped) {
+            rb_ary_behead(args->rest, args->rest_index);
+        }
+        else {
+            int len = rest_len(args);
+            if (args->rest_index == len) {
+                /* instead of dup + clear */
+                args->rest = Qfalse;
+            }
+            else {
+                args->rest = rb_ary_subseq(args->rest, args->rest_index, len);
+                args->rest_dupped = TRUE;
+            }
+        }
+        args->rest_index = 0;
+    }
+}
+
+static inline void
 args_copy(struct args_info *args)
 {
+    args_rest_behead(args);
+
     if (args->rest != Qfalse) {
 	int argc = args->argc;
 	args->argc = 0;
@@ -232,8 +257,11 @@ args_rest_array(struct args_info *args)
 {
     VALUE ary;
 
+    args_rest_behead(args);
+
     if (args->rest) {
-        ary = rb_ary_behead(args->rest, args->rest_index);
+        arg_rest_dup(args);
+        ary = args->rest;
         args->rest_index = 0;
 	args->rest = 0;
     }
@@ -961,7 +989,7 @@ setup_parameters_complex(rb_execution_context_t * const ec, const rb_iseq_t * co
 	args_setup_rest_parameter(args, locals + iseq->body->param.rest_start);
     }
 
-    VM_ASSERT(args->rest_index || nb(args) == 0);
+    VM_ASSERT((args_rest_behead(args), nb(args) == 0));
 
     if (iseq->body->param.flags.has_kw) {
 	VALUE * const klocals = locals + iseq->body->param.keyword->bits_start - iseq->body->param.keyword->num;
